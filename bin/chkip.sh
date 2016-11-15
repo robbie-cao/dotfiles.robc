52f15492491789f6a2193d3f8c591167c1969be9
Robbie Cao <robbie.cao@gmail.com>
2016-11-15 12:44:05 +0800

    Change hardcode file name to variable
    Signed-off-by: Robbie Cao <robbie.cao@gmail.com>

--- bin/chkip.sh
+++ bin/chkip.sh
@@ -3,11 +3,12 @@
 function exip() { lynx --dump http://ipecho.net/plain; }
 
 prev_ip=0
+tmp_file=.ip_addr
 
-if [ -e ip_addr ]; then
-    prev_ip=`cat ip_addr`
+if [ -e $tmp_file ]; then
+    prev_ip=`cat $tmp_file`
 else
-    echo "0.0.0.0" > ip_addr
+    echo "0.0.0.0" > $tmp_file
 fi
 
 curr_ip=`exip`
@@ -16,8 +17,7 @@ echo $prev_ip
 echo $curr_ip
 
 if [ $prev_ip != $curr_ip ]; then
-    mail -s "New IP: $curr_ip" robbie.cao@gmail.com robbie_cao@muabaobao.com yfwang@muabaobao.com < ip_addr
-    echo $curr_ip > ip_addr
+    mail -s "New IP: $curr_ip" robbie.cao@gmail.com < $tmp_file
+    echo $curr_ip > $tmp_file
 fi
-
 
