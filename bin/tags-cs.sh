6a8e1865870089888eb545857b7823cac997f05a
Robbie Cao <robbie.cao@arm.com>
2021-03-31 11:20:32 +0800

    Add tags-cs.sh
    Signed-off-by: Robbie Cao <robbie.cao@arm.com>

---
+++ bin/tags-cs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Generate cscope files
+
+echo > cscope.files
+
+find `pwd` -type f                         \
+    -iname '*.[ch]' -o -name '*.cpp'       \
+    -o -name '*.java' -o -name '*.aidl'    \
+    -o -name '[Mm]akefile' -o -name '*.mk' \
+    -o -name '*.sh'                        \
+    -o -name '*.s51'                       \
+    -o -name '*.xml'                       \
+    >> cscope.files
+
+cscope -qkb -i cscope.files
+
