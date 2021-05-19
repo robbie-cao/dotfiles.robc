907f7b5739edfcc292e99695a3142ca5ea684483
Robbie Cao <robbie.cao@arm.com>
2021-05-19 18:35:28 +0800

    Update tags.sh
    Signed-off-by: Robbie Cao <robbie.cao@arm.com>

--- bin/tags-cs.sh
+++ bin/tags-cs.sh
@@ -7,6 +7,7 @@ find `pwd` -type f                         \
     -iname '*.[ch]' -o -name '*.cpp'       \
     -o -name '*.java' -o -name '*.aidl'    \
     -o -name '[Mm]akefile' -o -name '*.mk' \
+    -o -name '*.S'                         \
     -o -name '*.sh'                        \
     -o -name '*.s51'                       \
     -o -name '*.xml'                       \
