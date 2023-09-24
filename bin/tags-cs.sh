9e62c48641880d55c5b380b608271b27fb4e20cb
Robbie Cao <robbie.cao@arm.com>
2023-09-24 22:19:32 +0800

    tags-cs: Add src_dir arguement
    Signed-off-by: Robbie Cao <robbie.cao@arm.com>

--- bin/tags-cs.sh
+++ bin/tags-cs.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 # Generate cscope files
 
+src_dir=${PWD}
+
+if [[ $# -gt 1 ]] ; then
+    src_dir="$1"
+fi
+
+if [[ ! -d "$src_dir" ]]; then
+    echo "$src_dir does not exist."
+    exit 1
+fi
+
 echo > cscope.files
 
-find `pwd` -type f                         \
+find $src_dir -type f                      \
     -iname '*.[ch]' -o -name '*.cpp'       \
     -o -name '*.java' -o -name '*.aidl'    \
     -o -name '[Mm]akefile' -o -name '*.mk' \
